2c31d23daf76c3a1774480dda5b8912a5ec44e74
Luis Carlos <luiscarlosnors@gmail.com>
2022-09-08 21:22:29 -0300

    feat: add header home.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import { Flex, Grid, Image } from '@chakra-ui/react'
+
+export function Header() {
+  return (
+    <Flex
+      bg="white"
+      w="100%"
+      as="header"
+      mx="auto"
+      px="1rem"
+      h={['50px', '100px']}
+      align="center"
+      justify="center"
+    >
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxW="1160px"
+        alignItems="center"
+        templateColumns="repeat(3, 1fr)"
+        justifyContent="center"
+        alignSelf="start"
+      >
+        <Image
+          w={['81px', '184px']}
+          src="/Logo.svg"
+          alt="Logo"
+          justifySelf="center"
+          gridColumn="2"
+        />
+      </Grid>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { Flex } from '@chakra-ui/react'
+import { Header } from '../components/Header'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Flex direction="column">
+      <Header />
+    </Flex>
   )
 }
